0e85a6020854b144930fad19ebb3c7b24f964df2
Tashinga Musanhu <44523844+Tashsub@users.noreply.github.com>
2020-08-25 21:19:16 +0200

    Login screen implemented

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
 #the front end willbe designed using kivy. CXonnect using Builder 
 #where as the logic will be in python
@@ -10,9 +12,23 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 Builder.load_file('design.kv')
 
 class  LoginScreen(Screen):
-    pass
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
 
+class SignUpScreen(Screen):
+    def add_user(self, name, passw):
 
+      
+        full_day = datetime.now().strftime("%d-%m-%Y")
+        with open("users.json") as file:
+            users = json.load(file)
+
+        users[name] = {'username': name, 'password': passw,'created at':full_day}
+        print(users)
+
+        with open("users.json", 'w') as file: 
+            json.dump(users, file)
+        
 class RootWidget(ScreenManager): 
     pass
 
