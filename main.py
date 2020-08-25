295b5141ca4ba8f378cdf8be7477316c2ce2ef16
Tashinga Musanhu <44523844+Tashsub@users.noreply.github.com>
2020-08-25 22:29:30 +0200

    first layer of error handling

--- main.py
+++ main.py
@@ -11,14 +11,29 @@ from datetime import datetime
 
 Builder.load_file('design.kv')
 
-class  LoginScreen(Screen):
+class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
+
+     #add functions to check if the user exists when trying to login
+    def login(self, name, passw):
+        with open("users.json", 'r') as file:
+            users = json.load(file) 
+        #checking username exists match it against the password stored
+        #for that username
+        person = users[name]
+        stored_password = person["password"]
+        if name in users and stored_password == passw: 
+            self.manager.current = "login_success"
+        else: 
+            self.ids.error_login.text = "Password or Username is incorrect. Try Again"
+
+        
+
 
 class SignUpScreen(Screen):
     def add_user(self, name, passw):
 
-      
         full_day = datetime.now().strftime("%d-%m-%Y")
         with open("users.json") as file:
             users = json.load(file)
@@ -28,6 +43,19 @@ class SignUpScreen(Screen):
 
         with open("users.json", 'w') as file: 
             json.dump(users, file)
+        self.manager.current = "sign_up_success"
+
+        
+class SignUpSuccess(Screen):
+    def back_to_mainpage(self):
+        self.manager.current = "login_screen"
+
+class LoginSuccess(Screen):
+    def logout(self):
+        self.manager.transition.direction = "left"
+        self.manager.current = "login_screen"
+
+    #def get_word(self, word):
         
 class RootWidget(ScreenManager): 
     pass
